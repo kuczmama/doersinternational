2a59b14484d02d4a38c6c44ebcf3c671ceb10302
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-01-08 22:09:19 +0900

    Action mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,3 +4,5 @@ class HomeController < ApplicationController
     @group = MeetupRequestService.group
   end
 end
+
+# ActionMailer::Base.mail(from: "mark@mkucz.com", to: "markkuczmarski@gmail.com", subject: "Test from a rails server!!", body: "Test").deliver_now
